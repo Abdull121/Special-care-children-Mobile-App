f521c9f2f17297c061a6ce59dca6f6edb08fc3b9
Muhammad Abdullah <93944428+Abdull121@users.noreply.github.com>
2025-02-27 04:12:30 +0500

    push notification

--- services/NotificationService.js
+++ services/NotificationService.js
@@ -1,5 +1,5 @@
 import * as Notifications from 'expo-notifications';
-import * as Device from 'expo-device';
+// import * as Device from 'expo-device';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
