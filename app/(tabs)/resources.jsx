2877984b418f46cb66db88ba324d124a87049f6c
Muhammad Abdullah <93944428+Abdull121@users.noreply.github.com>
2025-03-06 02:09:47 +0500

    resources screen

--- app/(tabs)/resources.jsx
+++ app/(tabs)/resources.jsx
@@ -6,14 +6,17 @@ import axios from "axios";
 import YouTubeCard from "../../components/YoutubeCard";
 import DoctorCard from "../../components/DoctorCard";
 import uuid from 'react-native-uuid';
+import Constants from 'expo-constants'
 
-const API_KEY = "AIzaSyBBeBYqvW-6nXKKmCULCWryT9lbOB3rtOA";
-const place_API='AlzaSyj0kKkKjuoLhLaeWgasee1OxuQjwsTxeXI';
+const API_KEY = Constants.expoConfig.extra.YOUTUBE_API_KEY;
+const place_API= Constants.expoConfig.extra.PLACE_API;
 const SEARCH_QUERY = "Parenting special needs children Urdu";
 
 const Resources = () => {
+ 
   const [videos, setVideos] = useState([]);
   const [clinics, setClinics] = useState([]);
+  const [school, setSchool] = useState([]);
   const [loading, setLoading] = useState(true);
   const [locationLoading, setLocationLoading] = useState(false);
 
@@ -33,11 +36,10 @@ const Resources = () => {
 
       let location = await Location.getCurrentPositionAsync({});
       fetchNearbyClinics(location.coords.latitude, location.coords.longitude);
+      fetchNearbySchool(location.coords.latitude, location.coords.longitude);
     } catch (error) {
       console.error('Location error:', error);
-    } finally {
-      setLocationLoading(false);
-    }
+    } 
   };
 
   const fetchNearbyClinics = async (lat, lon) => {
@@ -49,7 +51,7 @@ const Resources = () => {
     );
 
       
-      console.log('Overpass Response:', response.data.results);
+      //console.log('Overpass Response:', response.data.results);
   
       // Process the response
       const clinicsData = response.data.results.map((element) => {
@@ -66,10 +68,10 @@ const Resources = () => {
         return { name, address, lat, lon,rating, id, openHours};
       });
 
-    console.log('Processed Clinics:', clinicsData);
+    //console.log('Processed Clinics:', clinicsData);
 
     const filteredClinics= clinicsData.sort(() => Math.random() - 0.5).slice(0, 3);
-    console.log(filteredClinics)
+    //console.log(filteredClinics)
       
 
   
@@ -79,6 +81,52 @@ const Resources = () => {
       console.error('Clinic search error:', error);
      
       
+    }
+    finally {
+      setLocationLoading(false);
+    }
+  };
+
+
+  const fetchNearbySchool = async (lat, lon) => {
+    try {
+      
+
+    const response = await axios.get(
+      `https://maps.gomaps.pro/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=5000&type=school&keyword=special care children&key=${place_API}`
+    );
+
+      
+      console.log('school results:', response);
+  
+      // Process the response
+      const schoolData = response.data.results.map((element) => {
+        // console.log('Element:', element, index);
+        
+        let name = element.name ||  "Child Specialist Clinic";
+        let address = element.vicinity
+        let rating = element.rating
+        let lat = element.geometry?.location?.lat
+        let lon = element.geometry?.location?.lng
+        let id = uuid.v4(); // Generates a unique ID
+        let openHours= element.opening_hours?.open_now;
+  
+        return { name, address, lat, lon,rating, id, openHours};
+      });
+
+    console.log('Processed School:', schoolData);
+
+    const filteredSchool= schoolData.sort(() => Math.random() - 0.5).slice(0, 3);
+    
+        setSchool(filteredSchool);
+      // console.log('Final Clinics:', clinicsData);
+    } catch (error) {
+      console.error('Clinic search error:', error);
+     
+      
+    }
+    finally {
+      setLocationLoading(false);
     }
   };
 
@@ -115,7 +163,9 @@ const Resources = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", paddingVertical: 40 }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View className="flex-1 px-3 bg-white">
+        <View
+        // style={{ borderBlockColor: "blue", borderWidth: 1,  }}
+         className="flex-1 px-3 pb-16 bg-white">
           <Text className="text-2xl font-bold mb-3 text-black">Resources</Text>
           <Text className="text-blue-500 mb-3">Recommended Videos</Text>
 
@@ -136,7 +186,7 @@ const Resources = () => {
 
           <Text className="text-blue-500 my-3">Recommended Doctors</Text>
           
-          {locationLoading ? (
+          {locationLoading && clinics ? (
             <View className="flex items-center justify-center h-40">
               <ActivityIndicator size="large" color="blue" />
             </View>
@@ -154,9 +204,30 @@ const Resources = () => {
               }
             />
           )}
+
+          <Text className="text-blue-500 my-3">Recommended Schools</Text>
+          
+          {locationLoading && clinics ? (
+            <View className="flex items-center justify-center h-40">
+              <ActivityIndicator size="large" color="blue" />
+            </View>
+          ) : (
+            <FlatList
+              data={school}
+              keyExtractor={(item,) => item.id}
+              renderItem={({ item }) => <DoctorCard clinic={item} />}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerClassName="px-2"
+              ItemSeparatorComponent={() => <View style={{ width: 20 }} />}
+              ListEmptyComponent={
+                <Text className="text-gray-500">No school found nearby</Text>
+              }
+            />
+          )}
+
+
         </View>
-       
-        
        
       </ScrollView>
     </SafeAreaView>
